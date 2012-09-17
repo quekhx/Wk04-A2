2ff5c51e980c593d58425c56a6f5325432d58520
QUEK HUI XIN <quekqueksg@yahoo.com.sg>
2012-09-17 19:45:18 +0800

    IR V1

---
+++ Version 1/odd_even_led/odd_even_led.ino
@@ -0,0 +1,40 @@
+// Project done with
+// QUEK HUI XIN A0077319X
+// YU QING A0104694A
+// WONG DING CHUEN U097830N
+//17/9/2012
+
+
+//control the led's on/off by the odd/even number on the remote control
+
+#include <IRremote.h>
+ 
+int RECV_PIN = 11;
+ 
+IRrecv irrecv(RECV_PIN);
+ 
+decode_results results;
+ 
+void setup()
+{
+  Serial.begin(9600);
+  irrecv.enableIRIn(); //初始化红外遥控
+  pinMode(12,1);
+}
+ 
+void loop() {
+  if (irrecv.decode(&results)) {
+ if(results.value % 2 == 1) //odd number
+  {
+  digitalWrite(12,1);                
+   Serial.println(results.value); 
+   }
+  else if(results.value % 2 == 0) //even number
+  {
+   digitalWrite(12,0);           
+    Serial.println(results.value);   
+  }
+    irrecv.resume();
+  
+}
+}
